e5b434a564bffbd50f8675b54f7941b68c00740c
shalina2 <shalom.belaineh@gmail.com>
2019-01-23 17:12:46 -0800

    setup

---
+++ WebApplication1/Models/Personoftheyear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Personoftheyear
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+    }
+
+    public static List<Personoftheyear> GetPersonoftheyears(int startYear,int endyear)
+    {
+       string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../wwwroot/personOfTheYear.csv");
+        string personData = File.ReadAllText(path);
+        List<string> lists = new List<string>();
+
+}
+}
+
