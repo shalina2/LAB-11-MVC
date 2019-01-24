cc793220e34b0ff32d1348426819f439cb4926f0
shalina2 <shalom.belaineh@gmail.com>
2019-01-23 21:55:18 -0800

    Debugging

--- WebApplication1/Controllers/Homecontroller.cs
+++ WebApplication1/Controllers/Homecontroller.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class Homecontroller
+    public class Homecontroller:Controller
     {
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Index(int startYear, int endYear)
+
+        {
+            return RedirectToAction("Result", new { startYear, endYear });
+        }
+
+        [HttpGet]
+
+        public IActionResult Results(int startYear, int endYear)
+
+        {
+            return View(Personoftheyear.GetPersons(startYear, endYear));
+        }
     }
 }
