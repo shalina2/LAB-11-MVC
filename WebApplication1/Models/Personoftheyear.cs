c9a497509f9ea8746e946bb8d6a90edb6dbb8da7
shalina2 <shalom.belaineh@gmail.com>
2019-01-25 14:35:38 -0800

    done

--- WebApplication1/Models/Personoftheyear.cs
+++ WebApplication1/Models/Personoftheyear.cs
@@ -17,48 +17,49 @@ namespace WebApplication1.Models
         public string Title { get; set; }
         public string Category { get; set; }
         public string Context { get; set; }
-
+       // public static IEnumerable<object> PersonData { get; private set; }
 
         public static List<Personoftheyear> GetPersons(int startYear, int endyear)
         {
             List<Personoftheyear> PersonData = new List<Personoftheyear>();
 
-            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../wwwroot/personOfTheYear.csv");
-            var personData = File.ReadAllText(path);
-            List<string> lists = new List<string>();
+            string Filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../wwwroot/personOfTheYear.csv");
+            var lines = File.ReadAllLines(Filepath).Skip(1);
+            //List<string> lists = new List<string>();
 
             //converting string
-            foreach (var line in personData.Split('\n'))
-            {
-                lists.Add(line);
-            }
-            lists.RemoveAt(0);
+            //foreach (var item in line.Split('\n'))
+            //{
+            //    lists.Add(line);
+            //}
+            //lists.RemoveAt(0);
 
 
 
-            foreach (var item in PersonData)
+            foreach (var item in lines)
             { 
-                if (startYear >= 1927 && endyear <= 2016)
-                {
+               // if (startYear >= 1927 && endyear <= 2016)
+               // {
 
-                    string[] lists = item.split(',');
+                    string[] columns = item.Split(',');
                     Personoftheyear person = new Personoftheyear();
-                    person.Year = (lists[0] == "" ? 0 : Convert.ToInt32(lists[0]));
-                    person.Honor = lists[1];
-                    person.Name = lists[2];
-                    person.Country = lists[3];
-                    person.BirthYear = (lists[4] == "" ? 0 : Convert.ToInt32(lists[4]));
-                    person.DeathYear = (lists[5] == "" ? 0 : Convert.ToInt32(lists[5]));
-                    person.Title = lists[6];
-                    person.Category = lists[7];
-                    person.Context = lists[8];
-                    personData.Add(person);
+                    person.Year = (columns[0] == "" ? 0 : Convert.ToInt32(columns[0]));
+                    person.Honor = columns[1];
+                    person.Name = columns[2];
+                    person.Country = columns[3];
+                    person.BirthYear = (columns[4] == "" ? 0 : Convert.ToInt32(columns[4]));
+                    person.DeathYear = (columns[5] == "" ? 0 : Convert.ToInt32(columns[5]));
+                    person.Title = columns[6];
+                    person.Category = columns[7];
+                    person.Context = columns[8];
+                PersonData.Add(person);
                 }
 
+                List<Personoftheyear> listofPeople = PersonData.Where(p => (p.Year >= startYear) && (p.Year <= endyear)).ToList();
+                return listofPeople;
             }
-            List<Personoftheyear> listofPeople = personData.Where(p => (p.Year >= startYear) && (p.Year <= endyear)).ToList();
-            return listofPeople;
+           
         }
     }
-}
+
 
